c2bd43e8443b41aed7a1c0a0102c1d9273ce2718
Melih Yanar <melih.yanar@hotmail.com>
2021-02-08 20:25:02 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] itemkey;
+        TValue[] itemvalue;
+        public MyDictionary() 
+        {
+            itemkey = new TKey[0];
+            itemvalue = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempkey = itemkey;
+            TValue[] tempvalue = itemvalue;
+            itemkey = new TKey[itemkey.Length + 1];
+            itemvalue = new TValue[itemvalue.Length + 1];
+            for (int i = 0; i < tempkey.Length; i++)
+            {
+                itemkey[i] = tempkey[i];
+            }
+            for (int i = 0; i < tempvalue.Length; i++)
+            {
+                itemvalue[i] = tempvalue[i];
+            }
+            itemkey[itemkey.Length - 1] = key;
+            itemvalue[itemvalue.Length - 1] = value;
+        }
+        public TValue[] Items
+        {
+            get { return itemvalue; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+            myDictionary.Add(1, "Can");
+            myDictionary.Add(2, "Yanar");
+            foreach (var name in myDictionary.Items)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
